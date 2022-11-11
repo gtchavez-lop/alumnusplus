467c067f88624cb7747def54a903e9c7d9334a71
million23 <chavezgerald23@gmail.com>
2022-11-11 13:37:09 +0800

    added dynamic user page template

--- pages/finder/index.js
+++ pages/finder/index.js
@@ -2,179 +2,175 @@ import { FiArrowRight, FiLoader } from "react-icons/fi";
 import { useEffect, useState } from "react";
 
 import { AnimatePresence } from "framer-motion";
+import Link from "next/link";
 import { __PageTransition } from "../../lib/animtions";
 import __supabase from "../../lib/supabase";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 
 const Finder = () => {
-  const router = useRouter();
-  const [userCoorinates, setUserCoordinates] = useState();
-  const [suggestedUsers, setSuggestedUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState();
+	const router = useRouter();
+	const [userCoorinates, setUserCoordinates] = useState();
+	const [suggestedUsers, setSuggestedUsers] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState();
 
-  // const getDeviceLocation = () => {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       setUserCoordinates({
-  //         lat: position.coords.latitude,
-  //         lng: position.coords.longitude,
-  //       });
-  //     });
-  //   } else {
-  //     alert("Geolocation is not supported by this browser.");
-  //   }
-  // };
+	// const getDeviceLocation = () => {
+	//   if (navigator.geolocation) {
+	//     navigator.geolocation.getCurrentPosition((position) => {
+	//       setUserCoordinates({
+	//         lat: position.coords.latitude,
+	//         lng: position.coords.longitude,
+	//       });
+	//     });
+	//   } else {
+	//     alert("Geolocation is not supported by this browser.");
+	//   }
+	// };
 
-  const getSuggestedUsers = async () => {
-    const user = await __supabase.auth.user();
-    // get 10 random users
-    const { data: users, error } = await __supabase
-      .from("random_hunters")
-      .select("*")
-      .eq("address_city", user.user_metadata.city)
-      .limit(10);
+	const getSuggestedUsers = async () => {
+		const user = await __supabase.auth.user();
+		// get 10 random users
+		const { data: users, error } = await __supabase
+			.from("random_hunters")
+			.select("*")
+			.eq("address_city", user.user_metadata.city)
+			.limit(10);
 
-    if (error) {
-      console.log(error);
-    } else {
-      const filtered = users.filter((e) => e.email !== user.email);
-      setSuggestedUsers(filtered);
-      setTimeout(() => {
-        setLoading(false);
-      }, users.length * 200);
-      // getDeviceLocation();
-    }
-  };
+		if (error) {
+			console.log(error);
+		} else {
+			const filtered = users.filter((e) => e.email !== user.email);
+			setSuggestedUsers(filtered);
+			setTimeout(() => {
+				setLoading(false);
+			}, users.length * 200);
+			// getDeviceLocation();
+		}
+	};
 
-  const checkUser = async () => {
-    const user = __supabase.auth.user();
-    if (!user) {
-      router.push("/");
-    } else {
-      // getDeviceLocation();
-      setUser(user);
-      getSuggestedUsers();
-    }
-  };
+	const checkUser = async () => {
+		const user = __supabase.auth.user();
+		if (user) {
+			// getDeviceLocation();
+			setUser(user);
+			getSuggestedUsers();
+		} else {
+			router.push("/");
+		}
+	};
 
-  useEffect(() => {
-    checkUser();
-  }, []);
+	useEffect(() => {
+		checkUser();
+	}, []);
 
-  return (
-    <>
-      <motion.main
-        variants={__PageTransition}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-      >
-        <AnimatePresence mode="wait">
-          {loading && user && (
-            <motion.div
-              key={loading}
-              variants={__PageTransition}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              className="fixed top-0 left-0 flex flex-col justify-center items-center w-full min-h-screen"
-            >
-              <motion.div
-                animate={{ scale: [0, 1], opacity: [0, 0.5, 0] }}
-                transition={{
-                  duration: 2,
-                  ease: "easeInOut",
-                  repeat: Infinity,
-                }}
-                className="border-2 border-primary rounded-full h-64 w-64 absolute "
-              />
-              <motion.div
-                animate={{ scale: [0, 1], opacity: [0, 0.5, 0] }}
-                transition={{
-                  duration: 2,
-                  ease: "easeInOut",
-                  repeat: Infinity,
-                  delay: 1,
-                }}
-                className="border-2 border-primary rounded-full h-64 w-64 absolute "
-              />
-              <motion.img
-                animate={{ scale: [1.1, 1, 1.1] }}
-                transition={{
-                  duration: 1,
-                  ease: "easeInOut",
-                  repeat: Infinity,
-                }}
-                src={`https://avatars.dicebear.com/api/bottts/${user.user_metadata.username}.svg`}
-                className="w-24 h-24 rounded-full bg-base-300 z-10 shadow-lg"
-              />
-              <p className="absolute bottom-10">Searching</p>
-            </motion.div>
-          )}
-        </AnimatePresence>
+	return (
+		<>
+			<motion.main variants={__PageTransition} initial="initial" animate="animate" exit="exit">
+				<AnimatePresence mode="wait">
+					{loading && user && (
+						<motion.div
+							key={loading}
+							variants={__PageTransition}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							className="fixed top-0 left-0 flex flex-col justify-center items-center w-full min-h-screen"
+						>
+							<motion.div
+								animate={{ scale: [0, 1], opacity: [0, 0.5, 0] }}
+								transition={{
+									duration: 2,
+									ease: "easeInOut",
+									repeat: Infinity,
+								}}
+								className="border-2 border-primary rounded-full h-64 w-64 absolute "
+							/>
+							<motion.div
+								animate={{ scale: [0, 1], opacity: [0, 0.5, 0] }}
+								transition={{
+									duration: 2,
+									ease: "easeInOut",
+									repeat: Infinity,
+									delay: 1,
+								}}
+								className="border-2 border-primary rounded-full h-64 w-64 absolute "
+							/>
+							<motion.img
+								animate={{ scale: [1.1, 1, 1.1] }}
+								transition={{
+									duration: 1,
+									ease: "easeInOut",
+									repeat: Infinity,
+								}}
+								src={`https://avatars.dicebear.com/api/bottts/${user.user_metadata.username}.svg`}
+								className="w-24 h-24 rounded-full bg-base-300 z-10 shadow-lg"
+							/>
+							<p className="absolute bottom-10">Searching</p>
+						</motion.div>
+					)}
+				</AnimatePresence>
 
-        <AnimatePresence mode="wait">
-          {!loading && user && (
-            <motion.main
-              variants={__PageTransition}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              key={loading}
-              className="pb-16 lg:pt-24 pt-36"
-            >
-              <div className="flex flex-col justify-center items-center">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
-                  {suggestedUsers.map((user, index) => (
-                    <motion.div
-                      animate={{ y: [10, 0], opacity: [0, 1] }}
-                      transition={{
-                        duration: 0.5,
-                        ease: "easeInOut",
-                        delay: index * 0.1,
-                      }}
-                      key={user.user_id}
-                      className="group flex flex-col w-full md:w-auto text-center justify-center items-center bg-base-300 rounded-box p-4 py-3 cursor-pointer"
-                    >
-                      <img
-                        src={`https://avatars.dicebear.com/api/bottts/${user.user_id}.svg`}
-                        alt="profile"
-                        className="w-20 h-20 rounded-full bg-base-100 mt-5"
-                      />
-                      <p className="mt-5 text-xs font-bold opacity-50">
-                        @{user.username}
-                      </p>
-                      <h1 className="font-semibold text-sm">
-                        {user.first_name} {user.last_name}
-                      </h1>
-                      <p className="text-xs opacity-50">{user.city}</p>
+				<AnimatePresence mode="wait">
+					{!loading && user && (
+						<motion.main
+							variants={__PageTransition}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							key={loading}
+							className="pb-16 lg:pt-24 pt-36"
+						>
+							<div className="flex flex-col justify-center items-center">
+								<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
+									{suggestedUsers.map((user, index) => (
+										<Link href={`/hunter/${user.username}`} key={`user_${key}`}>
+											<motion.div
+												animate={{ y: [10, 0], opacity: [0, 1] }}
+												transition={{
+													duration: 0.5,
+													ease: "easeInOut",
+													delay: index * 0.1,
+												}}
+												key={user.id}
+												className="group flex flex-col w-full md:w-auto text-center justify-center items-center bg-base-300 rounded-box p-4 py-3 cursor-pointer"
+											>
+												<img
+													src={`https://avatars.dicebear.com/api/bottts/${user.username}.svg`}
+													alt="profile"
+													className="w-20 h-20 rounded-full bg-base-100 mt-5"
+												/>
+												<p className="mt-5 text-xs font-bold opacity-50">@{user.username}</p>
+												<h1 className="font-semibold text-sm">
+													{user.first_name} {user.last_name}
+												</h1>
+												<p className="text-xs opacity-50">{user.city}</p>
 
-                      <p className="hidden lg:flex text-primary text-sm w-full items-center gap-2 mt-10 opacity-20 justify-between font-bold group-hover:opacity-100 transition-all duration-200">
-                        Visit Profile <FiArrowRight />
-                      </p>
+												<p className="hidden lg:flex text-primary text-sm w-full items-center gap-2 mt-10 opacity-20 justify-between font-bold group-hover:opacity-100 transition-all duration-200">
+													Visit Profile <FiArrowRight />
+												</p>
 
-                      <button className="btn btn-primary lg:hidden mt-5 w-full gap-5">
-                        Visit Profile <FiArrowRight />
-                      </button>
-                    </motion.div>
-                  ))}
-                </div>
+												<button className="btn btn-primary lg:hidden mt-5 w-full gap-5">
+													Visit Profile <FiArrowRight />
+												</button>
+											</motion.div>
+										</Link>
+									))}
+								</div>
 
-                {suggestedUsers.length < 1 && (
-                  <div className="flex flex-col h-64 justify-center items-center">
-                    <p className="font-bold text-xl">No users found</p>
-                    <p className="text-sm">Try again later</p>
-                  </div>
-                )}
-              </div>
-            </motion.main>
-          )}
-        </AnimatePresence>
-      </motion.main>
-    </>
-  );
+								{suggestedUsers.length < 1 && (
+									<div className="flex flex-col h-64 justify-center items-center">
+										<p className="font-bold text-xl">No users found</p>
+										<p className="text-sm">Try again later</p>
+									</div>
+								)}
+							</div>
+						</motion.main>
+					)}
+				</AnimatePresence>
+			</motion.main>
+		</>
+	);
 };
 
 export default Finder;
--- pages/hunter/[username].js
+++ pages/hunter/[username].js
@@ -1,39 +1,154 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
 import { FiLoader } from "react-icons/fi";
+import Image from "next/image";
+import { __PageTransition } from "../../lib/animtions";
 import __supabase from "../../lib/supabase";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSelect } from "react-supabase";
 
-const HunterPage = () => {
-  const router = useRouter();
-  const { username } = router.query;
+export const getServerSideProps = async (context) => {
+	const { username } = context.params;
 
-  const [
-    { data: hunterData, loading: hunterLoading, error: hunterError },
-    reexecuteHunter,
-  ] = useSelect("user_hunters", {
-    columns: "*",
-    filter: (query) => query.eq("username", username),
-    options: { count: "exact" },
-  });
+	const { data: hunterData, error: hunterError } = await __supabase
+		.from("user_hunters")
+		.select("*")
+		.single()
+		.eq("username", username);
 
-  if (hunterLoading) {
-    return (
-      <>
-        <main className="fixed top-0 left-0 w-full h-screen flex flex-col justify-center items-center">
-          <FiLoader className="text-4xl animate-spin" />
-        </main>
-      </>
-    );
-  }
+	if (hunterError) {
+		return {
+			notFound: true,
+		};
+	}
 
-  return (
-    <>
-      <main className="pt-28">
-        <h1>{username}</h1>
-      </main>
-    </>
-  );
+	return {
+		props: {
+			hunterData,
+		},
+	};
+};
+
+const HunterPage = ({ hunterData }) => {
+	const [hunterPosts, setHunterPosts] = useState([]);
+	const [tabSelected, setTabSelected] = useState("posts");
+
+	const fetchHunterPosts = async () => {
+		const { data: posts, error } = await __supabase
+			.from("hunt_blog")
+			.select("*")
+			.eq("uploader_email", hunterData.email);
+
+		if (error) {
+			console.log(error);
+			return;
+		}
+		console.log(posts);
+
+		setHunterPosts(posts);
+	};
+
+	useEffect(() => {
+		fetchHunterPosts();
+	}, []);
+
+	// const [
+	//   { data: hunterData, loading: hunterLoading, error: hunterError },
+	//   reexecuteHunter,
+	// ] = useSelect("user_hunters", {
+	//   columns: "*",
+	//   filter: (query) => query.eq("username", username),
+	//   options: { count: "exact" },
+	// });
+
+	// if (hunterLoading) {
+	//   return (
+	//     <>
+	//       <main className="fixed top-0 left-0 w-full h-screen flex flex-col justify-center items-center">
+	//         <FiLoader className="text-4xl animate-spin" />
+	//       </main>
+	//     </>
+	//   );
+	// }
+
+	return (
+		<>
+			<motion.main
+				variants={__PageTransition}
+				initial="initial"
+				animate="animate"
+				exit="exit"
+				className="pt-36 lg:pt-28 max-w-2xl mx-auto "
+			>
+				{/* profile highlights */}
+				<div className="flex flex-col lg:flex-row ">
+					<div className="flex gap-5">
+						<Image
+							src={`https://avatars.dicebear.com/api/bottts/${hunterData.username}.svg`}
+							width={100}
+							height={100}
+							className="rounded-full border-4 border-primary p-2 border-opacity-10"
+							alt="avatar"
+						/>
+						<div className="flex flex-col justify-between">
+							<div className="flex gap-2">
+								<h1 className="text-2xl font-semibold">{hunterData.username}</h1>
+							</div>
+							<div className="flex gap-2">
+								<button className="btn btn-primary btn-sm ">Follow</button>
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col mt-5 lg:mt-0 lg:ml-10">
+						<p className="text-lg lg:text-2xl">
+							{hunterData.firstName} {hunterData.lastName}
+						</p>
+						<p className="text-sm opacity-30">{hunterData.email}</p>
+					</div>
+				</div>
+
+				{/* tabs */}
+				<div className="grid grid-cols-2 mt-16">
+					<div
+						onClick={() => setTabSelected("posts")}
+						className={`flex justify-center items-center border-b-4 py-3 cursor-pointer ${
+							tabSelected === "posts" ? "border-primary" : "border-transparent"
+						}`}
+					>
+						<p className="text-lg font-semibold">Posts</p>
+					</div>
+					<div
+						onClick={() => setTabSelected("connections")}
+						className={`flex justify-center items-center border-b-4 py-3 cursor-pointer ${
+							tabSelected === "connections" ? "border-primary" : "border-transparent"
+						}`}
+					>
+						<p className="text-lg font-semibold">Connections</p>
+					</div>
+				</div>
+
+				{/* posts */}
+				<AnimatePresence mode="wait">
+					{tabSelected === "posts" && (
+						<div>
+							{hunterPosts.length < 1 && (
+								<motion.div
+									variants={__PageTransition}
+									initial="initial"
+									animate="animate"
+									exit="exit"
+									className="flex flex-col justify-center items-center mt-10"
+								>
+									<p className="text-lg font-semibold">No posts yet</p>
+								</motion.div>
+							)}
+						</div>
+					)}
+				</AnimatePresence>
+			</motion.main>
+		</>
+	);
 };
 
 export default HunterPage;
