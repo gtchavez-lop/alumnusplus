520fec369b3341101af7c4f2e36f74d2720c6fbd
million23 <chavezgerald23@gmail.com>
2022-12-14 11:24:46 +0800

    supabase fix

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANONKEY;
 
 export const __supabase = createClient(supabaseURL, supabaseKey);
