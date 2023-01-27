5f70520d1914153c73651f55061528c47b1b1807
million23 <chavezgerald23@gmail.com>
2023-01-28 01:27:20 +0800

    Gerald - Jan28 Update Fix 1

--- pages/test.js
+++ pages/test.js
@@ -1,61 +1,6 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
-
 import { __supabase } from "@/supabase";
-import { useEffect } from "react";
-import { useUser } from "@supabase/auth-helpers-react";
 
 const TestPage = () => {
-  const localUser = useUser();
-
-  const getUserDetails = async () => {
-    const { data, error } = await __supabase
-      .from("user_hunters")
-      .select("*")
-      .eq("id", localUser.id)
-      .single();
-
-    if (error) {
-      console.log("error", error);
-      return null;
-    }
-
-    return data;
-  };
-
-  const currentUser = useQuery({
-    queryKey: ["currentUser"],
-    queryFn: getUserDetails,
-    enabled: !!localUser,
-  });
-
-  const getUserConnections = async () => {
-    const userConnections = currentUser.data.connections;
-
-    const { data, error } = await __supabase
-      .from("public_posts")
-      .select("*")
-      .in("uploader", [...userConnections, localUser.id]);
-
-    if (error) {
-      console.log("error", error);
-      return [];
-    }
-
-    return data;
-  };
-
-  const [feedList] = useQueries({
-    queries: [
-      {
-        queryKey: ["connections"],
-        queryFn: getUserConnections,
-        enabled: !!currentUser.isSuccess,
-      },
-    ],
-  });
-
-  console.log("connections", feedList);
-
   return <div className="py-32"></div>;
 };
 
