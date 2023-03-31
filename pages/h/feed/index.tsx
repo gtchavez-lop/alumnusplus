6701c4c3f65340f268804fe874c236c80c33f8d9
million23 <chavezgerald23@gmail.com>
2023-03-31 14:42:33 +0800

    blog feed fix

--- pages/h/feed/index.tsx
+++ pages/h/feed/index.tsx
@@ -48,82 +48,79 @@ const FeedPage = () => {
 	];
 
 	const fetchHunterFeed = async () => {
-		const connections: string[] = _currentUser.connections.concat(
-			_currentUser.id,
-		);
+		const connections = _currentUser.connections.concat(_currentUser.id);
 
 		const { data, error } = await supabase
 			.from("public_posts")
-			.select(
-				"id,content,comments,createdAt,updatedAt,uploader(id,email,full_name,username,avatar_url,is_verified),upvoters",
-			)
+			.select("*,uploader:uploader(*)")
 			.order("createdAt", { ascending: false })
 			.in("uploader", connections)
 			.eq("draft", false);
 
 		if (error) {
 			console.log("error", error);
-			return [];
+			return [] as THunterBlogPost[];
 		}
 
 		return data as THunterBlogPost[];
 	};
 
-	const fetchProvisionerFeed = async () => {
+	const fetchProvFeed = async () => {
 		const followingList = _currentUser.followedCompanies;
 
 		const { data, error } = await supabase
 			.from("public_provposts")
-			.select("id,content,createdAt,type,upvoters,uploader:uploader(*)")
+			.select("*,uploader:uploader(*)")
 			.order("createdAt", { ascending: false })
 			.in("uploader", followingList)
-			.eq("type", "provblog");
+			.eq("type", "provblog")
+			.eq("draft", false);
 
 		if (error) {
 			console.log("error", error);
-			return [];
+			return [] as TProvBlogPost[];
 		}
 
 		return data as TProvBlogPost[];
 	};
 
 	const fetchRecommendedUsers = async () => {
-		const localConnection = [_currentUser.connections, _currentUser.id];
-		const convertedMapToString = localConnection.map((item) => {
-			return item.toString();
-		});
-		const joined = convertedMapToString.join(",");
+		const connections = _currentUser.connections.concat(_currentUser.id);
 
 		const { data, error } = await supabase
 			.from("new_recommended_hunters")
 			.select("*")
-			.not("id", "in", `(${joined})`)
 			.limit(5);
 
-		if (error || localConnection.length === 0) {
-			return [];
+		if (error) {
+			console.log("error", error);
+			return [] as IUserHunter[];
 		}
 
-		return data;
+		const filtered = data.filter(
+			(user: { id: string }) => !connections.includes(user.id),
+		);
+
+		return filtered as IUserHunter[];
 	};
 
-	const [feedList, recommendedUsers, provFeedList] = useQueries({
+	const [hunterFeed, provFeed, recommendedUsers] = useQueries({
 		queries: [
 			{
-				queryKey: ["mainFeedList"],
+				queryKey: ["hunterFeed"],
 				queryFn: fetchHunterFeed,
+				enabled: !!_currentUser,
+				refetchOnWindowFocus: false,
+			},
+			{
+				queryKey: ["provFeed"],
+				queryFn: fetchProvFeed,
 				enabled: !!_currentUser,
 				refetchOnWindowFocus: false,
 			},
 			{
 				queryKey: ["recommendedUsers"],
 				queryFn: fetchRecommendedUsers,
-				enabled: !!_currentUser,
-				refetchOnWindowFocus: false,
-			},
-			{
-				queryKey: ["provFeedList"],
-				queryFn: fetchProvisionerFeed,
 				enabled: !!_currentUser,
 				refetchOnWindowFocus: false,
 			},
@@ -166,268 +163,253 @@ const FeedPage = () => {
 			toast("This post is a draft. You can edit it in your profile");
 		}
 
-		feedList.refetch();
+		hunterFeed.refetch();
 		setIsMakingPost(false);
 	};
 
 	return (
 		<>
-			{!_currentUser ||
-				feedList.isLoading ||
-				recommendedUsers.isLoading ||
-				(provFeedList.isLoading && (
-					<motion.main
-						variants={AnimPageTransition}
-						initial="initial"
-						animate="animate"
-						exit="exit"
-						className="relative w-full min-h-screen flex flex-col justify-center items-center"
-					>
-						<FiLoader className="text-lg" />
-						<p>Loading Feed</p>
-					</motion.main>
-				))}
-
-			{_currentUser &&
-				feedList.isSuccess &&
-				recommendedUsers.isSuccess &&
-				provFeedList.isSuccess && (
+			<motion.main
+				variants={AnimPageTransition}
+				initial="initial"
+				animate="animate"
+				exit="exit"
+				className="relative w-full min-h-screen grid grid-cols-1 lg:grid-cols-5 gap-4 pt-24 pb-36"
+			>
+				{_currentUser ? (
 					<>
-						<motion.main
-							variants={AnimPageTransition}
-							initial="initial"
-							animate="animate"
-							exit="exit"
-							className="relative w-full grid grid-cols-1 lg:grid-cols-5 gap-4 pt-24 pb-36"
+						{/* feed */}
+						<div
+							className="col-span-full lg:col-span-3 w-full"
+							ref={mainFeed_ui}
 						>
-							{/* feed */}
-							<div
-								className="col-span-full lg:col-span-3 w-full"
-								ref={mainFeed_ui}
-							>
-								{/* create post */}
-								<div ref={mainFeed_ui} className="flex gap-2 w-full relative">
-									{!isMakingPost ? (
-										<>
-											<div className=" relative w-12 h-12">
-												<Image
-													src={_currentUser.avatar_url}
-													alt="avatar"
-													className="bg-primary mask mask-squircle"
-													fill
-												/>
-											</div>
+							{/* create post */}
+							<div ref={mainFeed_ui} className="flex gap-2 w-full relative">
+								{!isMakingPost ? (
+									<>
+										<div className=" relative w-12 h-12">
+											<Image
+												src={_currentUser.avatar_url}
+												alt="avatar"
+												className="bg-primary mask mask-squircle"
+												fill
+											/>
+										</div>
 
+										<input
+											placeholder="What's on your mind?"
+											readOnly
+											onClick={() => setIsMakingPost(true)}
+											className="input input-primary rounded-full w-full"
+										/>
+									</>
+								) : (
+									<form onSubmit={handlePost} className="w-full">
+										<textarea
+											placeholder="What's on your mind?"
+											rows={10}
+											name="content"
+											className="textarea textarea-primary w-full"
+										/>
+										<p className="text-xs opacity-75 text-right">Markdown</p>
+										<label className="mt-3 flex items-center gap-2">
 											<input
-												placeholder="What's on your mind?"
-												readOnly
-												onClick={() => setIsMakingPost(true)}
-												className="input input-primary rounded-full w-full"
+												type="checkbox"
+												name="isDraft"
+												className="toggle toggle-primary"
 											/>
-										</>
-									) : (
-										<form onSubmit={handlePost} className="w-full">
-											<textarea
-												placeholder="What's on your mind?"
-												rows={10}
-												name="content"
-												className="textarea textarea-primary w-full"
-											/>
-											<p className="text-xs opacity-75 text-right">Markdown</p>
-											<label className="mt-3 flex items-center gap-2">
-												<input
-													type="checkbox"
-													name="isDraft"
-													className="toggle toggle-primary"
-												/>
-												<span>Save as Draft</span>
-											</label>
-											<div className="flex justify-end gap-2">
-												<button
-													type="reset"
-													onClick={() => setIsMakingPost(false)}
-													className="btn btn-ghost"
-												>
-													Cancel
-												</button>
-												<button type="submit" className="btn btn-primary">
-													Post
-												</button>
-											</div>
-										</form>
-									)}
-								</div>
-
-								{/* desktop tabs */}
-								<Tabs
-									tabs={feedTabs}
-									activeTab={feedTab}
-									onTabChange={(tab) => {
-										selectedFeedTab(tab as "hunter" | "provisioner");
-									}}
-								/>
-								{/* mobile select */}
-								<div className="lg:hidden mt-5 w-full">
-									<select
-										className="select select-bordered w-full "
-										value={feedTab}
-										onChange={(e) => {
-											selectedFeedTab(
-												e.target.value as "hunter" | "provisioner",
-											);
-										}}
-									>
-										{feedTabs.map((tab) => (
-											<option key={tab.value} value={tab.value}>
-												{tab.title}
-											</option>
-										))}
-									</select>
-								</div>
-
-								{/* feed list */}
-								<div className="mt-10 overflow-y-hidden" ref={tabContent}>
-									{feedTab === "hunter" && (
-										<div className="flex flex-col gap-5" ref={feedList_ui}>
-											{feedList.isLoading && (
-												<div className="py-10 flex flex-col">
-													<FiLoader className="animate-spin duration-500 text-3xl self-center" />
-													<p className="text-center w-full self-center max-w-xs">
-														Loading feed... This may take a while.
-													</p>
-												</div>
-											)}
-
-											{feedList.isSuccess &&
-												feedList.data.map((item: THunterBlogPost) => {
-													if (!item.draft) {
-														return (
-															<FeedCard
-																blogData={item}
-																key={item.id}
-																refetchData={feedList.refetch}
-															/>
-														);
-													}
-												})}
+											<span>Save as Draft</span>
+										</label>
+										<div className="flex justify-end gap-2">
+											<button
+												type="reset"
+												onClick={() => setIsMakingPost(false)}
+												className="btn btn-ghost"
+											>
+												Cancel
+											</button>
+											<button type="submit" className="btn btn-primary">
+												Post
+											</button>
 										</div>
-									)}
-									{feedTab === "provisioner" && (
-										<div className="flex flex-col gap-5" ref={feedList_ui}>
-											{provFeedList.isLoading && (
-												<div className="py-10 flex flex-col">
-													<FiLoader className="animate-spin duration-500 text-3xl self-center" />
-													<p className="text-center w-full self-center max-w-xs">
-														Loading feed... This may take a while.
-													</p>
-												</div>
-											)}
-
-											{provFeedList.isSuccess &&
-												provFeedList.data.map((item) => (
-													<ProvFeedCardGrid item={item} key={item.id} />
-												))}
-										</div>
-									)}
-
-									<div className="divider my-5 lg:my-0 lg:mt-5">
-										End of List
-									</div>
-								</div>
+									</form>
+								)}
 							</div>
 
-							{/* friend suggest and footer */}
-							<div className="col-span-full lg:col-span-2 flex flex-col gap-5">
-								{/* search for people */}
-								<form
-									onSubmit={(e) => {
-										e.preventDefault();
-										const formData = new FormData(e.currentTarget);
-
-										const searchQuery = formData.get("searchQuery");
-
-										if (!searchQuery) {
-											toast.error("Search query is required");
-											return;
-										}
-
-										router.push(`/h/search?query=${searchQuery}`);
+							{/* desktop tabs */}
+							<Tabs
+								tabs={feedTabs}
+								activeTab={feedTab}
+								onTabChange={(tab) => {
+									selectedFeedTab(tab as "hunter" | "provisioner");
+								}}
+							/>
+							{/* mobile select */}
+							<div className="lg:hidden mt-5 w-full">
+								<select
+									className="select select-bordered w-full "
+									value={feedTab}
+									onChange={(e) => {
+										selectedFeedTab(e.target.value as "hunter" | "provisioner");
 									}}
-									className="flex flex-row items-center gap-3"
 								>
-									<input
-										type="text"
-										name="searchQuery"
-										placeholder="Search for people"
-										className="input input-bordered flex-1"
-									/>
-									<button type="submit" className="btn btn-primary">
-										<MdSearch className="text-lg" />
-									</button>
-								</form>
+									{feedTabs.map((tab) => (
+										<option key={tab.value} value={tab.value}>
+											{tab.title}
+										</option>
+									))}
+								</select>
+							</div>
 
-								<div className="flex flex-col rounded-btn gap-3">
-									<p className="text-2xl font-bold">Suggested Connections</p>
-
-									{recommendedUsers.isLoading && (
-										<div className="flex flex-col gap-2">
-											{Array(5)
-												.fill("")
-												.map((_, index) => (
-													<motion.div
-														variants={AnimLoading}
-														animate="animate"
-														key={`recommendedloading_${index}`}
-														className="h-[72px] w-full bg-slate-500/50 rounded-btn animate-pulse"
-													/>
-												))}
-										</div>
-									)}
-
-									<div className="flex flex-col gap-2">
-										{recommendedUsers.isSuccess &&
-										recommendedUsers.data.length < 1 ? (
-											<p>
-												We can&apos;t find any recommended connections for you
-												at this moment.
-											</p>
-										) : (
-											<div className="flex flex-col gap-2">
-												{recommendedUsers.isSuccess &&
-													recommendedUsers.data.map((thisUser, index) => (
-														<Link
-															href={`/h?user=${thisUser.username}`}
-															key={`connection_${index}`}
-															className="flex gap-2 items-center justify-between p-2 hover:bg-base-200 rounded-btn"
-														>
-															<div className="flex gap-2 items-center">
-																<Image
-																	src={thisUser.avatar_url}
-																	alt="avatar"
-																	className="w-12 h-12 mask mask-squircle bg-primary object-center object-cover"
-																	width={48}
-																	height={48}
-																/>
-																<div>
-																	<p className="font-bold leading-none">
-																		{thisUser.full_name.first}{" "}
-																		{thisUser.full_name.last}
-																	</p>
-																	<p className="opacity-50 leading-none">
-																		@{thisUser.username}
-																	</p>
-																</div>
-															</div>
-														</Link>
-													))}
+							{/* feed list */}
+							<div className="mt-10 overflow-y-hidden" ref={tabContent}>
+								{feedTab === "hunter" && (
+									<div className="flex flex-col gap-5" ref={feedList_ui}>
+										{hunterFeed.isLoading && (
+											<div className="py-10 flex flex-col">
+												<FiLoader className="animate-spin duration-500 text-3xl self-center" />
+												<p className="text-center w-full self-center max-w-xs">
+													Loading feed... This may take a while.
+												</p>
 											</div>
 										)}
+
+										{hunterFeed.isSuccess &&
+											hunterFeed.data.map((item: THunterBlogPost) => {
+												if (!item.draft) {
+													return (
+														<FeedCard
+															blogData={item}
+															key={item.id}
+															refetchData={hunterFeed.refetch}
+														/>
+													);
+												}
+											})}
 									</div>
+								)}
+								{feedTab === "provisioner" && (
+									<div className="flex flex-col gap-5" ref={feedList_ui}>
+										{provFeed.isLoading && (
+											<div className="py-10 flex flex-col">
+												<FiLoader className="animate-spin duration-500 text-3xl self-center" />
+												<p className="text-center w-full self-center max-w-xs">
+													Loading feed... This may take a while.
+												</p>
+											</div>
+										)}
+
+										{provFeed.isSuccess &&
+											provFeed.data.map((item) => (
+												<ProvFeedCardGrid item={item} key={item.id} />
+											))}
+									</div>
+								)}
+
+								<div className="divider my-5 lg:my-0 lg:mt-5">End of List</div>
+							</div>
+						</div>
+
+						{/* friend suggest and footer */}
+						<div className="col-span-full lg:col-span-2 flex flex-col gap-5">
+							{/* search for people */}
+							<form
+								onSubmit={(e) => {
+									e.preventDefault();
+									const formData = new FormData(e.currentTarget);
+
+									const searchQuery = formData.get("searchQuery");
+
+									if (!searchQuery) {
+										toast.error("Search query is required");
+										return;
+									}
+
+									router.push(`/h/search?query=${searchQuery}`);
+								}}
+								className="flex flex-row items-center gap-3"
+							>
+								<input
+									type="text"
+									name="searchQuery"
+									placeholder="Search for people"
+									className="input input-bordered flex-1"
+								/>
+								<button type="submit" className="btn btn-primary">
+									<MdSearch className="text-lg" />
+								</button>
+							</form>
+
+							<div className="flex flex-col rounded-btn gap-3">
+								<p className="text-2xl font-bold">Suggested Connections</p>
+
+								{recommendedUsers.isLoading && (
+									<div className="flex flex-col gap-2">
+										{Array(5)
+											.fill("")
+											.map((_, index) => (
+												<motion.div
+													variants={AnimLoading}
+													animate="animate"
+													key={`recommendedloading_${index}`}
+													className="h-[72px] w-full bg-slate-500/50 rounded-btn animate-pulse"
+												/>
+											))}
+									</div>
+								)}
+
+								<div className="flex flex-col gap-2">
+									{recommendedUsers.isSuccess &&
+									recommendedUsers.data.length < 1 ? (
+										<p>
+											We can&apos;t find any recommended connections for you at
+											this moment.
+										</p>
+									) : (
+										<div className="flex flex-col gap-2">
+											{recommendedUsers.isSuccess &&
+												recommendedUsers.data.map((thisUser, index) => (
+													<Link
+														href={`/h?user=${thisUser.username}`}
+														key={`connection_${index}`}
+														className="flex gap-2 items-center justify-between p-2 hover:bg-base-200 rounded-btn"
+													>
+														<div className="flex gap-2 items-center">
+															<Image
+																src={thisUser.avatar_url}
+																alt="avatar"
+																className="w-12 h-12 mask mask-squircle bg-primary object-center object-cover"
+																width={48}
+																height={48}
+															/>
+															<div>
+																<p className="font-bold leading-none">
+																	{thisUser.full_name.first}{" "}
+																	{thisUser.full_name.last}
+																</p>
+																<p className="opacity-50 leading-none">
+																	@{thisUser.username}
+																</p>
+															</div>
+														</div>
+													</Link>
+												))}
+										</div>
+									)}
 								</div>
 							</div>
-						</motion.main>
+						</div>
 					</>
+				) : (
+					<div className="col-span-full my-10 flex flex-col items-center justify-center">
+						<p className="text-2xl font-bold">You are not logged in.</p>
+						<p className="text-center">
+							Please login to view your feed. If you don&apos;t have an account,
+							please register.
+						</p>
+					</div>
 				)}
+			</motion.main>
 		</>
 	);
 };
