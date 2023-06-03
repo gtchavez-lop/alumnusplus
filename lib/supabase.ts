fb96fbfcfd5ebe7619d69af8e629529a53d6365b
million23 <chavezgerald23@gmail.com>
2023-06-03 18:29:52 +0800

    updated supabase evnironment variable names

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANONKEY as string;
 
 const $supabase = createClient(supabaseURL, supabaseKey, {
 	auth: {
